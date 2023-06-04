e5d82f9bb307bc0abef1ce4f8fdaeb7db4100354
Maria del Mar <chocoinformatics2016@gmail.com>
2023-06-04 21:59:19 +0200

    fuck

--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import {
   Web3Button,
   useContract,
-  useCreateAuctionListing,
-  useCreateDirectListing,
   useStorageUpload,
   MediaRenderer,
+  useMintNFT,
+  useAddress,
+  MintNFTParams,
 } from "@thirdweb-dev/react";
-import {
-  MARKETPLACE_ADDRESS,
-  NFT_COLLECTION_ADDRESS,
-} from "../const/addresses";
+import { NFT_COLLECTION_ADDRESS } from "../const/addresses";
 import {
   Box,
   Flex,
@@ -27,6 +25,7 @@ import {
 import { useCallback } from "react";
 import { useForm } from "react-hook-form";
 import { useDropzone } from "react-dropzone";
+import { useRouter } from "next/router";
 
 type MetadataFormData = {
   name: string;
@@ -34,9 +33,27 @@ type MetadataFormData = {
   timeOn: string;
   timeOntype: string;
   material: string;
+  image: string;
 };
 
 function UploadForm() {
+  // Here we create the nft contract to interact with:
+  const { contract: nftCollection } = useContract(
+    NFT_COLLECTION_ADDRESS,
+    "CryptoUndies"
+  );
+  // Here we access to the mintNFT function from the contract.
+  const { mutateAsync: mintNft, isLoading, error } = useMintNFT(nftCollection);
+
+  //Here we access to the wallet that is currently conected:
+
+  const address = useAddress();
+
+  const addressString = address?.toString();
+
+  const router = useRouter();
+
+  //   this is the part to upload the image to IPFS
   const [uri, setUri] = useState<string[]>([]);
   const { mutateAsync: upload } = useStorageUpload();
   const onDrop = useCallback(
@@ -47,17 +64,27 @@ function UploadForm() {
     [upload]
   );
 
+  const image = uri[0];
+  typeof image === "string";
+  console.log(image);
+  const modifyImage = image?.replace("ipfs//", "https://ipfs.io/ipfs/");
+  console.log(modifyImage);
+
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
-  const { register, handleSubmit } = useForm<MetadataFormData>({
+  const { register, watch } = useForm<MetadataFormData>({
     defaultValues: {
       name: "",
       typeOfUnderwear: "",
       timeOn: "",
       timeOntype: "",
       material: "",
+      image: uri[0],
     },
   });
+
+  const typeOfUnderwear = watch("typeOfUnderwear");
+  console.log(typeOfUnderwear);
 
   return (
     <Flex justify="center">
@@ -90,44 +117,71 @@ function UploadForm() {
             <Text>Undies Traits!</Text>
             <Box>
               <FormLabel> Name:</FormLabel>
-              <Input type="text" />
+              <Input type="text" {...register("name")} />
             </Box>
             <Box>
               <FormControl>
                 <FormLabel>Type: </FormLabel>
-                <Select placeholder="Select option" size="md">
-                  <option value="option1">Panties</option>
-                  <option value="option2">Bra</option>
-                  <option value="option3">Boxers</option>
-                  <option value="option4">Socks </option>
+                <Select
+                  placeholder="Select option"
+                  size="md"
+                  {...register("typeOfUnderwear")}
+                >
+                  <option value="Panties">Panties</option>
+                  <option value="Bra">Bra</option>
+                  <option value="Boxers">Boxers</option>
+                  <option value="Socks">Socks </option>
                 </Select>
               </FormControl>
             </Box>
             <Box>
               <FormControl>
                 <FormLabel mt={2}>Time you had it on:</FormLabel>
-                <Input placeholder="0" size="md" type="number" />
+                <Input
+                  placeholder="0"
+                  size="md"
+                  type="number"
+                  {...register("timeOn")}
+                />
                 <Select placeholder="Select option" size="md">
-                  <option value="option1">Minutes</option>
-                  <option value="option2">Hours</option>
-                  <option value="option3">Days</option>
+                  <option value="Minutes">Minutes</option>
+                  <option value="Hours">Hours</option>
+                  <option value="Days">Days</option>
                 </Select>
               </FormControl>
             </Box>
             <Box>
               <FormControl>
                 <FormLabel mt={2}>Material:</FormLabel>
-                <Select placeholder="Select option" size="md">
-                  <option value="option1">Latex</option>
-                  <option value="option2">Leader</option>
-                  <option value="option3">Textil</option>
+                <Select
+                  placeholder="Select option"
+                  size="md"
+                  {...register("timeOntype")}
+                >
+                  <option value="Latex">Latex</option>
+                  <option value="Leader">Leader</option>
+                  <option value="Textil">Textil</option>
                 </Select>
               </FormControl>
             </Box>
             <Box>
-              <Web3Button contractAddress={NFT_COLLECTION_ADDRESS}>
-                Mint Now!
-              </Web3Button>
+              {address ? (
+                <Web3Button
+                  contractAddress={NFT_COLLECTION_ADDRESS}
+                  action={(nftCollection) =>
+                    nftCollection.erc721.mint({
+                      name: watch("name"),
+                      description: watch("typeOfUnderwear"),
+                      image:
+                        "https://ipfs.io/ipfs/QmbkSxnVXRd3AJi1MqLSQFvKbkBBrRsGcu2jpvjVWbGMjB/logo.png",
+                    })
+                  }
+                >
+                  Mint NFT
+                </Web3Button>
+              ) : (
+                <Text> Please conect your wallet!</Text>
+              )}
             </Box>
           </Stack>
         </form>
