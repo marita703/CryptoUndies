fd8150869741b85000c6877bd4e08ed872ec5228
Maria del Mar <chocoinformatics2016@gmail.com>
2023-06-04 14:55:56 +0200

    first commit

---
+++ Lib/NavBarButtonsData.tsx
@@ -0,0 +1,8 @@
+const NavBarButtonsData = [
+  { name: "Market Place", href: "/" },
+  { name: "CreateNFT", href: "/create" },
+  { name: "Collections", href: "/collections" },
+  { name: "MyNFTPortal", href: "/portal" },
+];
+
+export default NavBarButtonsData;
